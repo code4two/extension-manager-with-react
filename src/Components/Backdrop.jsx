43de88f3cfa94ad73c7c31a92996b1fbb0145a37
Peter Adepoju <peteradepoju22@gmail.com>
2025-09-02 15:36:18 +0100

    made changes

--- src/Components/Backdrop.jsx
+++ src/Components/Backdrop.jsx
@@ -1,5 +1,5 @@
 const Backdrop = () => {
-  return <div className="inset-0 fixed bg-gray-800 z-20 opacity-65" />;
+  return <div className="inset-0 fixed bg-gray-800 z-20 opacity-44" />;
 };
 
 export default Backdrop;
