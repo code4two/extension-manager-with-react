06fa123cf41457ca4c212a7f6b61bd4c566a7a9a
Peter Adepoju <peteradepoju22@gmail.com>
2025-09-07 19:41:09 +0100

    updated file

--- src/Components/layouts/MainLayout.jsx
+++ src/Components/layouts/MainLayout.jsx
@@ -1,22 +1,21 @@
 import { Link } from "react-router-dom";
+import { useTheme } from "../ThemeContext";
 
 const MainLayout = () => {
+  const { changeTheme } = useTheme();
+
   return (
     <div className="flex justify-between font-bold items-center my-6 flex-col sm:flex-row">
       <h1 className="text-4xl mb-2">Extensions List</h1>
       <ul className="flex items-center font-normal text-lg gap-3 sm:gap-2">
-        <li
-          className={`${
-            changeTheme === "dark" ? "bg-red-500" : " text-black"
-          } px-3 py-1 rounded-2xl cursor-pointer`}
-        >
+        <li className="px-3 py-1 rounded-2xl cursor-pointer bg-red-500">
           <Link to="/">All</Link>
         </li>
         <li
           className={`${
             changeTheme === "dark"
               ? "bg-slate-700 border-1 border-gray-500"
-              : "bg-white text-black border-0"
+              : "bg-white"
           } px-3 py-1 rounded-2xl cursor-pointer`}
         >
           <Link to="/active-extensions">Active</Link>
@@ -25,7 +24,7 @@ const MainLayout = () => {
           className={`${
             changeTheme === "dark"
               ? "bg-slate-700 border-1 border-gray-500"
-              : "bg-white text-black border-0"
+              : "bg-white"
           } px-3 py-1 rounded-2xl cursor-pointer`}
         >
           <Link to="/inactive-extensions">Inactive</Link>
