567073c4dba75536a141f8c8edc706c878a22698
Peter Adepoju <peteradepoju22@gmail.com>
2025-09-07 16:35:32 +0100

    added state

--- src/Components/layouts/MainLayout.jsx
+++ src/Components/layouts/MainLayout.jsx
@@ -5,13 +5,29 @@ const MainLayout = () => {
     <div className="flex justify-between font-bold items-center my-6 flex-col sm:flex-row">
       <h1 className="text-4xl mb-2">Extensions List</h1>
       <ul className="flex items-center font-normal text-lg gap-3 sm:gap-2">
-        <li className="bg-red-500 px-3 py-1 rounded-2xl cursor-pointer">
+        <li
+          className={`${
+            changeTheme === "dark" ? "bg-red-500" : " text-black"
+          } px-3 py-1 rounded-2xl cursor-pointer`}
+        >
           <Link to="/">All</Link>
         </li>
-        <li className="bg-slate-700 border-1 border-gray-500 px-3 py-1 rounded-2xl cursor-pointer">
+        <li
+          className={`${
+            changeTheme === "dark"
+              ? "bg-slate-700 border-1 border-gray-500"
+              : "bg-white text-black border-0"
+          } px-3 py-1 rounded-2xl cursor-pointer`}
+        >
           <Link to="/active-extensions">Active</Link>
         </li>
-        <li className="bg-slate-700 border-1 border-gray-500 px-3 py-1 rounded-2xl cursor-pointer">
+        <li
+          className={`${
+            changeTheme === "dark"
+              ? "bg-slate-700 border-1 border-gray-500"
+              : "bg-white text-black border-0"
+          } px-3 py-1 rounded-2xl cursor-pointer`}
+        >
           <Link to="/inactive-extensions">Inactive</Link>
         </li>
       </ul>
